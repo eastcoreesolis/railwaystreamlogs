c2572123c0478421c6d91ddafc3ac38442dd03b6
eastcoreesolis <55853625+eastcoreesolis@users.noreply.github.com>
2023-06-13 09:07:37 -0600

    Create logs.py

---
+++ logs.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+import subprocess
+import logging
+
+# Configure logging
+logging.basicConfig(level=logging.INFO, filename='railway_logs.txt', filemode='w',
+                    format='%(asctime)s - %(levelname)s - %(message)s')
+
+# Create a logger instance
+logger = logging.getLogger('RailwayLogger')
+
+# Stream logs to both console and file
+console_handler = logging.StreamHandler()
+file_handler = logging.FileHandler('railway_logs.txt')
+logger.addHandler(console_handler)
+logger.addHandler(file_handler)
+
+# Command to stream logs from Railway app deployment
+command = "railway logs -d"  # Replace with the actual command to stream logs
+
+# Start streaming logs
+process = subprocess.Popen(command.split(), stdout=subprocess.PIPE, universal_newlines=True)
+
+# Read logs from the process output and stream to file and console
+for log in process.stdout:
+    log = log.strip()
+    logger.info(log)
+    # You can add additional processing or filtering of the logs here if needed
+
+# Close the process and log file handlers
+process.stdout.close()
+process.wait()
+logging.shutdown()
